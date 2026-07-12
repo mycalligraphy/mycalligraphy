99125233ade0883e93904412e1c5ec84b279298d
mycalligraphy <s3334139@gmail.com>
2026-07-12 22:01:39 +0330

    Add files via upload

---
+++ asgi.py
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+"""
+ASGI config for calligraphy_site project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calligraphy_site.settings')
+
+application = get_asgi_application()
+=======
+"""
+ASGI config for calligraphy_site project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calligraphy_site.settings')
+
+application = get_asgi_application()
+>>>>>>> 7956767a86adae2d1e0b7eff85090f4301cedb4a
